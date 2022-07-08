8409f61ea3a53eb3ba353fc062b2c38e861c0187
Naomi Christie <naomi.christie@fundingcircle.com>
2022-07-08 12:23:47 +0100

    Add script to start game from

---
+++ start_game.sh
@@ -0,0 +1,7 @@
+docker-compose down
+docker-compose build
+docker-compose run --rm web python manage.py makemigrations
+docker-compose run --rm web python manage.py migrate
+docker-compose up -d
+sleep 5
+python3 -m webbrowser http://0.0.0.0:8000/games/
